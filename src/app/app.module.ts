d8b041461dc00d8f7d49eab2499b485db8d65fc2
nolwenn-dujardin <nolwenn35560@gmail.com>
2023-03-02 15:19:13 +0100

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MyComponentComponent } from './my-component/my-component.component';
+
+import { FormsModule } from '@angular/forms';
+import { FilterPokemonPipePipe } from './filter-pokemon--pipe.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyComponentComponent,
+    FilterPokemonPipePipe
+  ],
+  imports: [
+    FormsModule,
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
